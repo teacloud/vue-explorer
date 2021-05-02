95070f926fea379b492b821c88fb24baa31dfcc5
teacloud <vantra90@gmail.com>
2021-05-01 20:46:22 -0600

    stubbed agent service

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/agents">Agents</router-link> |
-
-    <router-link to="/about">About</router-link>
   </div>
   <router-view />
 </template>
---
+++ src/services/agent.service.ts
@@ -0,0 +1,27 @@
+import http from "../http-common";
+
+class AgentService {
+  getAll() {
+    return http.get("/agents");
+  }
+
+  get(id: string) {
+    return http.get(`/agents/${id}`);
+  }
+
+  create(data: any) {
+    return http.post("/agents", data);
+  }
+
+  update(id: string, data: any) {
+    return http.put(`/agents/${id}`, data);
+  }
+
+  findByName(name: string) {
+    return http.get(`/tutorials?name=${name}`);
+  }
+}
+
+
+
+export default new AgentService();
